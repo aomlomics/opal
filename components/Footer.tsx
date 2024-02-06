1503c9f4a5ee310fca766d62c6de8efeb67f519a
Carter Rollins <croll2198@gmail.com>
2024-02-06 14:03:12 -0500

    Adds footer text and fixes header image

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,9 @@
 export default function Footer() {
 	return (
 		<footer className="footer items-center p-4 bg-neutral text-neutral-content mt-auto z-30">
+			<div className="items-center grid-flow-col">
+				<p>Copyright © 2024 - All right reserved</p>
+			</div>
 		</footer>
 	);
 }
