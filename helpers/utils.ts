c309566ef9bea8fd433d79795bdf582081c6d1a3
Carter Rollins <croll2198@gmail.com>
2024-02-06 15:21:04 -0500

    Adds utility functions and updates Next requirement

--- helpers/utils.ts
+++ helpers/utils.ts
@@ -1,6 +1,17 @@
+export async function fetcher(url: string) {
+	const res = await fetch(url);
+	if (!res.ok) return { response: "error" };
+	return await res.json();
+}
+
 export function getBaseUrl() {
 	if (process.env.NODE_ENV === "development") {
 		return "http://localhost:3000";
 	}
 	//TODO: add prod URL here
+}
+
+export function isEmpty(obj: Object) {
+	for (const x in obj) { if (obj.hasOwnProperty(x))  return false; }
+   	return true;
 }
