26af70352d8031cd1bb3f780b801771d4d5de2cc
Carter Rollins <croll2198@gmail.com>
2024-02-06 13:40:04 -0500

    Removes boilerplate and adds structure for components

---
+++ components/Footer.tsx
@@ -0,0 +1,6 @@
+export default function Footer() {
+	return (
+		<footer className="footer items-center p-4 bg-neutral text-neutral-content mt-auto z-30">
+		</footer>
+	);
+}
---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+	return (
+		<header className="sticky top-0 z-40">
+
+		</header>
+	);
+}
