bf35f0b4b237805622413f51ba404b37cd2b71d6
Carter Rollins <croll2198@gmail.com>
2024-02-07 12:25:05 -0500

    Login styling

--- components/UserNav.tsx
+++ components/UserNav.tsx
@@ -5,12 +5,14 @@ import { useUser, UserButton, SignInButton } from "@clerk/nextjs";
 export default function UserNav() {
 	const { isSignedIn, isLoaded, user } = useUser();
 	return (
-		<>
+		<div className="m-3">
 			{isSignedIn ? (
 				<UserButton afterSignOutUrl="/"/>
 			) : (
-				<SignInButton></SignInButton>
+				<SignInButton>
+					<button className="btn">Sign In</button>
+				</SignInButton>
 			)}
-		</>
+		</div>
 	);
 }
