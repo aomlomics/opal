0553fd8825daa6334ce67cfeab546915854e6a9a
Bayden Willms <bwillms9@gmail.com>
2024-02-09 11:51:43 -0500

    Added NOAA AOML webpage and link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,10 @@
 export default function Footer() {
-	return (
-		<footer className="footer items-center p-4 text-neutral-content mt-auto z-30 bg-secondary">
-			<div className="items-center grid-flow-col">
-				<p>Copyright © 2024 - All right reserved</p>
+	return(
+		<footer className = "footer p-1 text-neutral-content mt-auto z-30 bg-secondary">
+			<div className = "text-left">
+				<p className="-mb-3">Copyright © 2024 - All Rights Reserved.</p>
+				<p className="text-neutral-content hover:underline"><a href="https://www.aoml.noaa.gov/">NOAA's Atlantic Oceanographic and Meteorological Laboratory</a></p>
 			</div>
 		</footer>
-	);
+	)
 }
