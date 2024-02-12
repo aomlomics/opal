7a2106bc458e2955994c84e06fb84ceba3c0f0a2
Carter Rollins <croll2198@gmail.com>
2024-02-12 17:10:32 -0500

    Adds openstreetmap map

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,12 @@
+import dynamic from 'next/dynamic'
+const Map = dynamic(() => import('@/components/Map'), {
+	ssr: false,
+})
+
 export default function Dashboard() {
 	return (
 		<>
-			
+			<Map></Map>
 		</>
 	);
 }
