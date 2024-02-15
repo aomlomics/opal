f1ff20baa342f206e6c389daa66a900762e9f865
Carter Rollins <croll2198@gmail.com>
2024-02-15 17:03:52 -0500

    Displays response from remote server

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,10 +3,19 @@ const Map = dynamic(() => import('@/components/Map'), {
 	ssr: false,
 })
 
-export default function Dashboard() {
+export default async function Dashboard() {
+	const res = await fetch("https://opalserver-qnwedardvq-uc.a.run.app/", {
+		method: "GET",
+		cache: "no-store"
+	});
+	const data = await res.json();
+
 	return (
 		<div className="w-full grow-1">
 			<Map></Map>
+			<div>
+				<p>Response from remote server: {data.response}</p>
+			</div>
 		</div>
 	);
 }
