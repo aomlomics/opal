ae93c2c082921e501f940fdc3f6136c45660a080
Carter Rollins <croll2198@gmail.com>
2024-02-16 11:52:38 -0500

    Adds https to production link

--- helpers/utils.ts
+++ helpers/utils.ts
@@ -8,7 +8,7 @@ export function getBaseUrl() {
 	if (process.env.NODE_ENV === "development") {
 		return "http://localhost:3000";
 	}
-	return "opal-ochre.vercel.app";
+	return "https://opal-ochre.vercel.app";
 }
 
 export function isEmpty(obj: Object) {
