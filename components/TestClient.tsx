6c422ce0ef9f3faaf8040ac82396ee3c59f2600c
Carter Rollins <croll2198@gmail.com>
2024-02-21 16:48:05 -0500

    Adds test client to demonstrate API routes

---
+++ components/TestClient.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { clientApiFetch as clientApiFetch } from "@/helpers/utils";
+import useSWR from "swr";
+
+
+export default function TestClient() {
+	const projectsResponse = clientApiFetch(useSWR, "projects");
+	if (projectsResponse.isLoading) return <div>Loading...</div>;
+  	if (projectsResponse.error) return <div>Error</div>;
+	const projects = projectsResponse.data;
+
+	return (
+		<p>
+			Response from API: {JSON.stringify(projects)}
+		</p>
+	);
+}
