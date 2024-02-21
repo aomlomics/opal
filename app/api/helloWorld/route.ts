6b03e625924018801b2150ec1146b726d80fc446
Carter Rollins <croll2198@gmail.com>
2024-02-21 16:55:35 -0500

    Fixes build script to use js Prisma generator

--- app/api/helloWorld/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "@/helpers/prisma";
-import type { Prisma } from "@prisma/client";
-
-export async function GET(request: NextRequest) {
-	const searchParams = request.nextUrl.searchParams;
-
-	return NextResponse.json({ response: "Hello World!" });
-}
