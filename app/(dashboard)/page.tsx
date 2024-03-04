08e729a31481a97cac59537fb6a93a6e649b6a22
Carter Rollins <croll2198@gmail.com>
2024-03-04 12:13:12 -0500

    Test

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -19,6 +19,7 @@ export default async function Dashboard() {
 		<div className="w-full grow-1">
 			<Map></Map>
 			<div>
+				<p>hi</p>
 				<p>Response from remote server: {data}</p>
 				<p>------------------------------------------------------</p>
 				<TestClient></TestClient>
