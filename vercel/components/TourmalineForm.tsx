d7a0a8cd0a1bc3ffdec75d7a2516ac1ee40649c5
Carter Rollins <croll2198@gmail.com>
2024-03-13 16:34:41 -0400

    Updates form to handle file inputs

--- vercel/components/TourmalineForm.tsx
+++ vercel/components/TourmalineForm.tsx
@@ -2,7 +2,8 @@
 
 
 /*
----Code changes to be made---
+---TODO---
+- Fix submit button enabling on tab switch
 - Make the tabs look better (on the left side)
 - Send file as well as the rest of the form data
 - Have fields that load only if a certain option is selected,
@@ -17,47 +18,51 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { getRemoteUrl } from '@/helpers/utils';
+import { serialize } from 'object-to-formdata';
 
 // Define the form schema using Zod
 const schema = z.object({
   denoiseMethod: z.enum(['DADA2 paired-end', 'DADA2 single-end', 'Deblur single-end']),
   taxonomicLevel: z.number().min(1).max(7).optional(), // Ensuring the number is between 1 and 7
-  // metadataFile does not need to be in the Zod schema since it's handled separately for the file upload
+  metadataFile: z.custom<FileList | Buffer>((data) => {
+	return typeof window === 'undefined' ? data instanceof Buffer : data instanceof FileList // Check if on client/server for NextJS
+  })
 });
 
-type FormData = z.infer<typeof schema> & {
-  metadataFile?: FileList;
-};
+type SchemaData = z.infer<typeof schema>;
 
 export default function TourmalineForm() {
   const [activeTab, setActiveTab] = useState('Denoise'); // Initial active tab
   const [formSubmitted, setFormSubmitted] = useState(false);
-  const { register, handleSubmit, formState: { isDirty, isValid, errors }, watch, setValue } = useForm<FormData>({
+  const { register, handleSubmit, formState: { isDirty, isValid, errors }, watch, setValue } = useForm<SchemaData>({
     resolver: zodResolver(schema),
     mode: 'onChange',
   });
 
   // Manually register the file input field for metadata file
-  React.useEffect(() => {
-    register('metadataFile');
-  }, [register]);
+//   React.useEffect(() => {
+//     register('metadataFile');
+//   }, [register]);
 
-  const onSubmit: SubmitHandler<FormData> = async (data) => {
-    const formData = new FormData();
-    formData.append('denoiseMethod', data.denoiseMethod); // Append denoiseMethod to FormData
-    if(data.taxonomicLevel !== undefined) { // Ensure taxonomicLevel is defined before appending
-      formData.append('taxonomicLevel', data.taxonomicLevel.toString()); // Convert taxonomicLevel to string and append
-    }
+  async function onSubmit(data: SchemaData) {
+	const body = serialize(data) // convert SchemaData object to FormData
+    // formData.append('denoiseMethod', data.denoiseMethod); // Append denoiseMethod to FormData
+    // if(data.taxonomicLevel !== undefined) { // Ensure taxonomicLevel is defined before appending
+    //   formData.append('taxonomicLevel', data.taxonomicLevel.toString()); // Convert taxonomicLevel to string and append
+    // }
 
     // Append metadata file to FormData if it exists
-    if (data.metadataFile) {
-      formData.append('metadataFile', data.metadataFile[0]); // Append the first file in the metadataFile FileList
-    }
+    // if (data.metadataFile) {
+    //   formData.append('metadataFile', data.metadataFile[0]); // Append the first file in the metadataFile FileList
+    // }
 
     try {
       const response = await fetch(`${getRemoteUrl()}/tourmalineReceive`, {
         method: "POST",
-        body: formData, // Send the FormData object as the body of the request
+		headers: {
+			"Content-Type": "multipart/form-data"
+		},
+        body // Send the FormData object as the body of the request
       });
 
       if (!response.ok) {
@@ -114,21 +119,25 @@ export default function TourmalineForm() {
         )}
         {activeTab === 'Metadata' && (
           <div>
-            <label htmlFor="metadataFile" className="mb-2 text-lg text-gray-700">Upload Metadata (.tsv):</label>
-            <input
-              type="file"
-              {...register('metadataFile')}
-              accept=".tsv"
-              onChange={(e) => setValue('metadataFile', e.target.files ? e.target.files : undefined)}
-              className="appearance-none bg-white border border-gray-300 w-full py-2 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            />
+			<label htmlFor="metadataFile" className="form-control w-full max-w-xs">
+				<div className="label">
+					<span className="label-text">Upload Metadata (.tsv):</span>
+				</div>
+				<input
+					type="file"
+					{...register('metadataFile')}
+					accept=".tsv"
+					// onChange={(e) => setValue('metadataFile', e.target.files ? e.target.files : undefined)}
+					className="file-input file-input-bordered w-full max-w-xs" />
+			</label>
           </div>
         )}
         <div className="flex justify-center">
           <button
             type="submit"
             disabled={!isDirty || !isValid || formSubmitted} // Submit button is disabled based on form state and submission status
-            className={`mt-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md ${!isDirty || !isValid || formSubmitted ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-700'} focus:outline-none text-white`}
+            // className={`mt-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md ${!isDirty || !isValid || formSubmitted ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-700'} focus:outline-none text-white`}
+			className="btn btn-primary"
           >
             Submit
           </button>
