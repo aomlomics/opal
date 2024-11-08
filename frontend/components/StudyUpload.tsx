00a444ddd955f538de91dcad58fab8dc578abc0a
Carter Rollins <carter.rollins@noaa.gov>
2024-11-08 00:18:27 -0500

    Testing

--- frontend/components/StudyUpload.tsx
+++ frontend/components/StudyUpload.tsx
@@ -5,12 +5,14 @@ import { useFormState } from "react-dom";
 
 export default function StudyUpload() {
 	//const [state, formAction] = useFormState(studyUploadAction, { message: "" });
-	function formAction() {
-		console.log("test")
-	}
 	const state = {
 		message: "",
 		error: ""
+	}
+
+	function formAction(formData: FormData) {
+		console.log("test")
+		studyUploadAction(state, formData);
 	}
 
 	return (
@@ -19,43 +21,43 @@ export default function StudyUpload() {
 				<div className="label">
 					<span className="label-text text-neutral-content">Study File:</span>
 				</div>
-				<input id="samplesFile" type="file" name="studyFile" required className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
+				<input id="samplesFile" type="file" name="studyFile" className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
 			</label>
 			<label className="form-control w-full max-w-xs">
 				<div className="label">
 					<span className="label-text text-neutral-content">Library File:</span>
 				</div>
-				<input id="samplesFile" type="file" name="libraryFile" required className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
+				<input id="samplesFile" type="file" name="libraryFile" className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
 			</label>
 			<label className="form-control w-full max-w-xs">
 				<div className="label">
 					<span className="label-text text-neutral-content">Samples File:</span>
 				</div>
-				<input id="samplesFile" type="file" name="samplesFile" required className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
+				<input id="samplesFile" type="file" name="samplesFile" className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
 			</label>
 			<label className="form-control w-full max-w-xs">
 				<div className="label">
 					<span className="label-text text-neutral-content">16S ASV:</span>
 				</div>
-				<input id="asvFile" type="file" name="16sAsvFile" required className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
+				<input id="asvFile" type="file" name="16sAsvFile" className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
 			</label>
 			<label className="form-control w-full max-w-xs">
 				<div className="label">
 					<span className="label-text text-neutral-content">16S Occurrences:</span>
 				</div>
-				<input id="asvFile" type="file" name="16sOccFile" required className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
+				<input id="asvFile" type="file" name="16sOccFile" className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
 			</label>
 			<label className="form-control w-full max-w-xs">
 				<div className="label">
 					<span className="label-text text-neutral-content">18S ASV:</span>
 				</div>
-				<input id="asvFile" type="file" name="18sAsvFile" required className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
+				<input id="asvFile" type="file" name="18sAsvFile" className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
 			</label>
 			<label className="form-control w-full max-w-xs">
 				<div className="label">
 					<span className="label-text text-neutral-content">18S Occurrences:</span>
 				</div>
-				<input id="asvFile" type="file" name="18sOccFile" required className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
+				<input id="asvFile" type="file" name="18sOccFile" className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
 			</label>
 			<button onClick={() => { state.message = ""; state.error = "" }} className="btn btn-secondary">Submit</button>
 			<span className="text-neutral-content">{state.message} {state.error}</span>
--- frontend/helpers/actions.ts
+++ frontend/helpers/actions.ts
@@ -82,6 +82,7 @@ function replaceDead(
 
 export async function studyUploadAction(prevState: FormState, formData: FormData) {
 	console.log("start");
+	return { message: "Success" };
 	try {
 		//Study file
 		const studyCol = {} as Record<string, string>;
