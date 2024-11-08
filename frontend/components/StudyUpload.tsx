6f1f0649f00272ee03300a112c84f3b2460032eb
Carter Rollins <carter.rollins@noaa.gov>
2024-11-08 00:49:01 -0500

    Adds error boundary to hopefully display issue

--- frontend/components/StudyUpload.tsx
+++ frontend/components/StudyUpload.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { studyUploadAction } from "@/helpers/actions";
-import { useFormState } from "react-dom";
+import { useFormState, useFormStatus } from "react-dom";
 
 export default function StudyUpload() {
 	const [state, formAction] = useFormState(studyUploadAction, { message: "" });
+	const { pending } = useFormStatus();
 
 	return (
 		<form className="card-body" action={formAction}>
@@ -12,46 +13,99 @@ export default function StudyUpload() {
 				<div className="label">
 					<span className="label-text text-neutral-content">Study File:</span>
 				</div>
-				<input id="samplesFile" type="file" name="studyFile" className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
+				<input
+					id="samplesFile"
+					type="file"
+					name="studyFile"
+					required
+					className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs"
+				/>
 			</label>
 			<label className="form-control w-full max-w-xs">
 				<div className="label">
 					<span className="label-text text-neutral-content">Library File:</span>
 				</div>
-				<input id="samplesFile" type="file" name="libraryFile" className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
+				<input
+					id="samplesFile"
+					type="file"
+					name="libraryFile"
+					required
+					className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs"
+				/>
 			</label>
 			<label className="form-control w-full max-w-xs">
 				<div className="label">
 					<span className="label-text text-neutral-content">Samples File:</span>
 				</div>
-				<input id="samplesFile" type="file" name="samplesFile" className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
+				<input
+					id="samplesFile"
+					type="file"
+					name="samplesFile"
+					required
+					className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs"
+				/>
 			</label>
 			<label className="form-control w-full max-w-xs">
 				<div className="label">
 					<span className="label-text text-neutral-content">16S ASV:</span>
 				</div>
-				<input id="asvFile" type="file" name="16sAsvFile" className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
+				<input
+					id="asvFile"
+					type="file"
+					name="16sAsvFile"
+					required
+					className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs"
+				/>
 			</label>
 			<label className="form-control w-full max-w-xs">
 				<div className="label">
 					<span className="label-text text-neutral-content">16S Occurrences:</span>
 				</div>
-				<input id="asvFile" type="file" name="16sOccFile" className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
+				<input
+					id="asvFile"
+					type="file"
+					name="16sOccFile"
+					required
+					className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs"
+				/>
 			</label>
 			<label className="form-control w-full max-w-xs">
 				<div className="label">
 					<span className="label-text text-neutral-content">18S ASV:</span>
 				</div>
-				<input id="asvFile" type="file" name="18sAsvFile" className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
+				<input
+					id="asvFile"
+					type="file"
+					name="18sAsvFile"
+					required
+					className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs"
+				/>
 			</label>
 			<label className="form-control w-full max-w-xs">
 				<div className="label">
 					<span className="label-text text-neutral-content">18S Occurrences:</span>
 				</div>
-				<input id="asvFile" type="file" name="18sOccFile" className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs" />
+				<input
+					id="asvFile"
+					type="file"
+					name="18sOccFile"
+					required
+					className="file-input file-input-bordered file-input-secondary bg-neutral-content w-full max-w-xs"
+				/>
 			</label>
-			<button onClick={() => { state.message = ""; state.error = "" }} className="btn btn-secondary">Submit</button>
-			<span className="text-neutral-content">{state.message} {state.error}</span>
+			<button
+				onClick={() => {
+					state.message = "";
+					state.error = "";
+				}}
+				className="btn btn-secondary"
+			>
+				Submit
+			</button>
+			{pending && <span className="text-neutral-content">Loading...</span>}
+			<span className="text-neutral-content">
+				{state.message} {state.error}
+			</span>
 		</form>
 	);
 }
