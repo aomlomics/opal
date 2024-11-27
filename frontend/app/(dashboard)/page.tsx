3e7b6a4be915e28db06270ed462304db3681a39a
Bayden Willms <bwillms9@gmail.com>
2024-11-27 02:46:59 -0500

    Welcome page update

--- frontend/app/(dashboard)/page.tsx
+++ frontend/app/(dashboard)/page.tsx
@@ -10,47 +10,49 @@ const Map = dynamic(() => import("@/app/components/Map"), {
 export default async function Home() {
 	return (
 		<main className="flex flex-col grow bg-base-400 text-base-content">
-			<div className="relative w-full h-[450px] bg-black overflow-hidden z-content-overlay">
+			<div className="relative w-full h-[80vh] bg-black overflow-hidden z-content-overlay">
 				<Image
 					src="/images/school_of_fish.jpg"
 					alt="Hero Image"
-					width={1600}
-					height={500}
+					width={1920}
+					height={1080}
 					priority
 					className="w-full h-full object-cover saturate-150"
 				/>
-				<div className="absolute inset-0 bg-black/10"></div>
-				<div className="absolute z-content text-left px-4 sm:px-6 lg:px-8 top-[30%]">
-					<h1 className="text-5xl sm:text-6xl md:text-7xl mb-2">
-						<span className="block text-primary animate-slide-in">Welcome</span>
+				{/* Reduced opacity in gradients by adjusting via-X/Y values */}
+				<div className="absolute inset-0 bg-gradient-to-r from-base-100 via-base-100/60 to-transparent animate-gradient-organic"></div>
+				<div className="absolute inset-0 bg-gradient-to-t from-base-100 via-base-100/10 to-transparent animate-gradient-organic"></div>
+				<div className="absolute inset-0 bg-gradient-to-r from-base-100 via-base-100/60 to-transparent"></div>
+				<div className="absolute inset-0 bg-gradient-to-t from-base-100 via-base-100/10 to-transparent"></div>
+				<div className="absolute z-content text-left px-4 sm:px-6 lg:px-8 top-1/4 max-w-xl">
+					<h1 className="text-4xl sm:text-5xl md:text-6xl mb-4 drop-shadow-lg">
+						<span className="block text-primary animate-slide-in font-light">Welcome</span>
 					</h1>
-					<div className="text-base-content">
-						<span className="block text-3xl sm:text-4xl md:text-5xl mb-2 text-gray-400">
+					<div className="text-base-content drop-shadow-lg">
+						<span className="block text-2xl sm:text-3xl md:text-4xl mb-4">
 							to the <span className="text-primary">NOAA Ocean DNA Explorer</span>
 						</span>
-						<div className="text-lg sm:text-xl mb-6 text-gray-400">
-							<span>NODE is a data sharing platform, search engine, and visualization and analysis tool</span>
+						<div className="text-lg sm:text-xl mb-8 text-base-content/90">
+							<span>a data sharing platform, search engine, and visualization and analysis tool</span>
 							<span> for ocean environmental DNA (eDNA) data</span>
 						</div>
 					</div>
 
-					<div className="flex flex-col items-start gap-2">
-						<a href="/data" className="btn btn-lg btn-secondary bg-primary outline-none text-white hover:bg-primary/80">
+					<div className="flex flex-col items-start gap-4">
+						<a href="/data" className="btn btn-lg btn-secondary bg-primary/90 backdrop-blur-sm outline-none text-white hover:bg-primary transition-all duration-300">
 							Start Searching
 						</a>
-						{/* <span className="text-lg sm:text-xl text-gray-300">
-							Or <span className="font-bold text-white">scroll down</span> to explore our data overview
-						</span> */}
 					</div>
 				</div>
 			</div>
 
-			<div className="h-[200px] relative">
+			<div className="relative mb-12 text-center">
+				<p className="text-primary mb-2">Explore Our Data</p>
 				<Link href="#dataSummary">
-					<div className="absolute top-4 left-1/2 transform -translate-x-1/2 z-modal animate-bounce">
+					<div className="animate-bounce">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
-							className="h-8 w-8"
+							className="h-8 w-12 text-primary mx-auto"
 							fill="none"
 							viewBox="0 0 24 24"
 							stroke="currentColor"
@@ -60,7 +62,7 @@ export default async function Home() {
 					</div>
 				</Link>
 			</div>
-			<div id="dataSummary" className="z-content px-4 sm:px-6 lg:px-8 pt-8 pb-12">
+			<div id="dataSummary" className="z-content px-4 sm:px-6 lg:px-8 pb-12">
 			<div className="mb-4 text-2xl text-base-content">Showing all 
 				<span className="text-primary"> Studies</span></div>
 				<div className="flex gap-8">
