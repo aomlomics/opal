a2101c3bb9ac761a23b4c283bd0752fa214a7f2c
Carter Rollins <carter.rollins@noaa.gov>
2025-01-28 13:35:47 -0500

    Fixes header logo

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
 							<NodeLogo
 								alt="NODE Logo"
 								fill={true}
-								className="object-contain"
+								style={{ objectFit: "contain" }}
 								priority={true}
 								sizes="(max-width: 768px) 100vw, 33vw"
 							/>
