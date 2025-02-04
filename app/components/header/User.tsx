25d5a6c5868f5025ec5084063023b591110b99cc
Bayden Willms <bwillms9@gmail.com>
2025-02-04 17:17:36 -0500

    Clerk userbutton component nearly styled correctly

--- app/components/header/User.tsx
+++ app/components/header/User.tsx
@@ -6,7 +6,36 @@ export default function User() {
 	return (
 		<>
 			<SignedIn>
-				<UserButton>
+				<UserButton
+					appearance={{
+						elements: {
+							cardBox: "bg-base-100 shadow-md",
+							headerTitle: "text-primary", // this doesnt do anything
+							headerSubtitle: "text-primary", // this one doesnt do anything
+							headerBackground: "bg-primary",
+							button: "text-primary hover:bg-primary",
+							buttonText: "text-primary",
+							divider: "bg-base-300",
+							
+							userButtonPopoverActionButtonIcon: "text-primary",
+							userButtonPopoverActionButtonLabel: "text-primary",
+							
+							// for the email name
+							userPreviewSecondaryIdentifier: "text-primary",
+							// for the name
+							userPreviewMainIdentifier: "text-primary",
+							
+							socialButtonsBlockButton: "bg-base-200 text-primary hover:bg-primary",
+							formButtonPrimary: "text-primary hover:bg-primary/80",
+							formFieldInput: "bg-base-100 text-primary",
+							
+							avatarBox: "border-primary",
+							
+							footer: "hidden",
+							userButtonPopoverFooter: "hidden"
+						}
+					}}
+				>
 					<UserButton.MenuItems>
 						<UserButton.Link href="/mySubmissions" label="My Submissions" labelIcon={<span>.</span>} />
 					</UserButton.MenuItems>
@@ -14,7 +43,7 @@ export default function User() {
 			</SignedIn>
 			<SignedOut>
 				<SignInButton>
-					<button className="btn bg-primary text-white hover:bg-primary/80">Sign In</button>
+					<button className="btn bg-primary text-base-content hover:bg-primary/80">Sign In</button>
 				</SignInButton>
 			</SignedOut>
 		</>
