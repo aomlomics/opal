baf82a97dbe7e18eae0b83881371d9a2ab122f41
Bayden Willms <bwillms9@gmail.com>
2025-02-04 12:32:14 -0500

    Adjusted layout of analysis page to match new project page

--- app/(dashboard)/explore/analysis/[analysis_run_name]/page.tsx
+++ app/(dashboard)/explore/analysis/[analysis_run_name]/page.tsx
@@ -39,17 +39,26 @@ export default async function Analysis_Run_name({ params }: { params: Promise<{ 
 	const { _count: _, Occurrences: __, ...justAnalysis } = analysis;
 
 	return (
-		<div className="max-w-7xl mx-auto p-6 space-y-8">
-			<header className="mb-8">
-				<h1 className="text-4xl font-semibold text-primary mb-2">{analysis_run_name}</h1>
-			</header>
+		<div className="max-w-7xl mx-auto p-6">
+			<div className="grid grid-cols-4 gap-8 mb-3">
+				<div className="col-span-4">
+					<header>
+						<h1 className="text-4xl font-semibold text-primary mb-2">{analysis_run_name}</h1>
+						<div className="bg-base-200 -ml-3.5">
+						<OccDownloadButton
+							text="Download Occurrence Table"
+							filename={`${analysis_run_name}_occurrenceTable`}
+							where={{ analysis_run_name }}
+						/>
+					</div>
+					</header>
+				</div>
 
-			<div className="grid grid-cols-3">
-				<div className="flex flex-col col-span-2">
-					<div className="flex">
+				<div className="col-span-2 space-y-4">
+					<div className="grid grid-cols-2 gap-4">
 						<Link
 							href={`/explore/project/${analysis.project_id}`}
-							className="stat bg-base-200 rounded-lg p-6 hover:bg-base-200/80 transition-colors"
+							className="stat bg-base-200 p-6 hover:bg-base-300 transition-colors"
 						>
 							<div className="text-sm font-medium text-base-content/70">Project</div>
 							<div className="text-2xl font-medium mt-1">{analysis.project_id}</div>
@@ -57,75 +66,80 @@ export default async function Analysis_Run_name({ params }: { params: Promise<{ 
 
 						<Link
 							href={`/explore/assay/${analysis.assay_name}`}
-							className="stat bg-base-200 rounded-lg p-6 hover:bg-base-200/80 transition-colors"
+							className="stat bg-base-200 p-6 hover:bg-base-300 transition-colors"
 						>
 							<div className="text-sm font-medium text-base-content/70">Assay</div>
 							<div className="text-2xl font-medium mt-1">{analysis.assay_name}</div>
 						</Link>
 					</div>
 
-					<div className="flex">
-						<Link href={`/`} className="stat bg-base-200 rounded-lg p-6 hover:bg-base-200/80 transition-colors">
+					<div className="grid grid-cols-2 gap-4">
+						<div className="stat bg-base-200 p-6">
 							<div className="text-sm font-medium text-base-content/70">Total Occurrences</div>
 							<div className="text-2xl font-medium mt-1">{analysis._count.Occurrences}</div>
-						</Link>
+						</div>
 
-						<Link href={`/`} className="stat bg-base-200 rounded-lg p-6 hover:bg-base-200/80 transition-colors">
+						<div className="stat bg-base-200 p-6">
 							<div className="text-sm font-medium text-base-content/70">Total Assignments</div>
 							<div className="text-2xl font-medium mt-1">{analysis._count.Assignments}</div>
-						</Link>
+						</div>
 					</div>
 				</div>
-				<div className="bg-base-200 rounded-lg p-6">
-					<h2 className="text-lg font-medium text-primary mb-4">Analysis Information</h2>
-					<div className="h-[400px]">
-						<DataDisplay data={justAnalysis} omit={["id", "project_id", "userId", "analysis_run_name", "assay_name"]} />
+
+				<div className="col-span-2">
+					<div className="bg-base-200 p-6 h-full">
+						<h2 className="text-lg font-medium text-base-content/70 mb-4">Analysis Information</h2>
+						<div className="h-[300px] overflow-y-auto">
+							<DataDisplay 
+								data={justAnalysis} 
+								omit={["id", "project_id", "userId", "analysis_run_name", "assay_name"]} 
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
 
-			<div className="mb-5">
-				<OccDownloadButton
-					text="Occurrence Table"
-					filename={`${analysis_run_name}_occurrenceTable`}
-					where={{ analysis_run_name }}
-				/>
-			</div>
+			<div className="mt-4 px-6">
+				<h2 className="card-title text-base-content/70 mb-4">Data Explorer:</h2>
+				<div role="tablist" className="tabs tabs-lifted">
+					<input type="radio" defaultChecked name="dataTabs" role="tab" className="tab" aria-label="Samples" />
+					<div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+						<div className="card-body p-0 overflow-hidden h-[400px]">
+							<Map
+								locations={analysis.Occurrences.map((samp) => ({ ...samp.Sample }))}
+								id="samp_name"
+								table="sample"
+								cluster
+							/>
+						</div>
+					</div>
 
-			<div role="tablist" className="tabs tabs-lifted">
-				<input type="radio" defaultChecked name="dataTabs" role="tab" className="tab" aria-label="Samples" />
-				<div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-					<div className="card-body p-0 overflow-hidden h-[400px]">
-						<Map
-							locations={analysis.Occurrences.map((samp) => ({ ...samp.Sample }))}
-							id="samp_name"
-							table="sample"
-							cluster
+					<input type="radio" name="dataTabs" role="tab" className="tab" aria-label="Assignments" />
+					<div
+						role="tabpanel"
+						className="tab-content bg-base-100 border-base-300 rounded-box p-6 h-[400px] w-full overflow-hidden"
+					>
+						<Table 
+							table="assignment" 
+							title="featureid" 
+							where={{ analysis_run_name }}
 						/>
 					</div>
-				</div>
 
-				<input type="radio" name="dataTabs" role="tab" className="tab" aria-label="Assignments" />
-				<div
-					role="tabpanel"
-					className="tab-content bg-base-100 border-base-300 rounded-box p-6 h-[400px] w-full overflow-hidden"
-				>
-					<Table table="assignment" title="featureid" where={{ analysis_run_name }}></Table>
-				</div>
-
-				<input type="radio" name="dataTabs" role="tab" className="tab" aria-label="Taxa" />
-				<div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box">
-					<TaxaGrid
-						take={25}
-						cols={5}
-						where={{
-							Assignments: {
-								some: {
-									analysis_run_name
+					<input type="radio" name="dataTabs" role="tab" className="tab" aria-label="Taxa" />
+					<div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box">
+						<TaxaGrid
+							take={25}
+							cols={5}
+							where={{
+								Assignments: {
+									some: {
+										analysis_run_name
+									}
 								}
-							}
-						}}
-					/>
+							}}
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
