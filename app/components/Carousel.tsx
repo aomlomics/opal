294f7818c1537110aa9716f87a0bd16e6e518e0b
Bayden Willms <bwillms9@gmail.com>
2025-02-17 16:12:36 -0500

    Some more background images for Welcome page

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -18,7 +18,15 @@ const carouselImgSrc = [
 	`/images/carousel/chimaera.jpg`,
 	`/images/carousel/fish_m.jpg`,
 	`/images/carousel/hydroid.jpg`,
-	`/images/carousel/pricklefish_m.jpg`
+	`/images/carousel/pricklefish_m.jpg`,
+	`/images/carousel/apr16_1.jpg`,
+	`/images/carousel/brain_coral.jpg`,
+	`/images/carousel/coral_florida.jpg`,
+	`/images/carousel/ex2206_dive03_medusa.jpg`,
+	`/images/carousel/jelly3.jpg`,
+	`/images/carousel/lancefish.jpg`,
+	`/images/carousel/silky_medusa_colonema_sericeum.jpg`,
+	`/images/carousel/siphonophore_800.jpg`
 ];
 
 export default function Carousel() {
